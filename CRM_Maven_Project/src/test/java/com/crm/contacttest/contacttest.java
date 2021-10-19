bfd065982cd4e047c7681ddefe98373f4ab9799c
GUDU <GUDU@DESKTOP-8A68LM6>
2021-10-19 14:50:54 +0530

    demo d

---
+++ CRM_Maven_Project/src/test/java/com/crm/contacttest/contacttest.java
@@ -0,0 +1,17 @@
+package com.crm.contacttest;
+
+import org.testng.annotations.Test;
+
+public class contacttest {
+	@Test
+	public void createcontacttest() {
+		System.out.println("Creates");
+		
+		
+	}
+@Test
+public void editContactTest() {
+	System.out.println("Creates");
+	
+}
+}
---
+++ CRM_Maven_Project/src/test/java/readdatafromcmd/DataRead.java
@@ -0,0 +1,14 @@
+package readdatafromcmd;
+
+import org.testng.annotations.Test;
+
+public class DataRead {
+	@Test
+	public void DataRead() {
+		String URL=System.getProperty("url");
+		System.out.println(URL);
+		
+		
+	}
+
+}
